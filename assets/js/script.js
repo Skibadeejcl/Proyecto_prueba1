cc717738e6987484e59a6bf8ccd6b7c776d04b61
Eduardo Castillo <eduardo.castillo.lara@outlook.com>
2022-01-13 16:07:12 -0300

    Tooltips y Modal agregados, además de detalles finos

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+$ (document).ready(function () {
+
+
+  $("a").click(function () {
+      var gato = this.hash
+
+      $("html, body").animate(
+          {
+              scrollTop: $(gato).offset().top
+          },
+          800
+      )
+
+  })
+
+})
+
+
+
+
+$(document).scroll( function (e) {
+        const y = $("html").scrollTop();
+        if(y > 300) $("nav").addClass("nav-black")
+        else $("nav").removeClass("nav-black")
+})
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
