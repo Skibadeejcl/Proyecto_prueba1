2f43a4ce3c333c757fedb94f706e4f07982b6e64
Eduardo Castillo <eduardo.castillo.lara@outlook.com>
2022-01-13 16:44:17 -0300

    Últimos detalles listos antes de subir a la web

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
+// Smooth Scroll
 $ (document).ready(function () {
-
 
   $("a").click(function () {
       var gato = this.hash
@@ -10,14 +10,11 @@ $ (document).ready(function () {
           },
           800
       )
-
   })
-
 })
 
 
-
-
+// Para ver en y>300 la barra en negro
 $(document).scroll( function (e) {
         const y = $("html").scrollTop();
         if(y > 300) $("nav").addClass("nav-black")
@@ -25,6 +22,7 @@ $(document).scroll( function (e) {
 })
 
 
+// Para que Tooltips funcione
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
